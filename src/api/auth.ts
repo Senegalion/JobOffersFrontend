0d49af257f2a813c3bc588881b5bc7ad21d5400b
Lucas <lupelikan@int.pl>
2025-04-05 20:16:00 +0300

    Added registration and login pages with forms

---
+++ src/api/auth.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8082";
+
+interface RegisterUserRequest {
+  username: string;
+  password: string;
+}
+
+interface LoginUserRequest {
+  username: string;
+  password: string;
+}
+
+interface JwtResponse {
+  username: string;
+  token: string;
+}
+
+export const registerUser = async (username: string, password: string) => {
+  try {
+    const response = await axios.post(`${API_URL}/register`, {
+      username,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Registration error:", error);
+    throw error;
+  }
+};
+
+export const loginUser = async (username: string, password: string) => {
+  try {
+    const response = await axios.post<JwtResponse>(`${API_URL}/token`, {
+      username,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Login error:", error);
+    throw error;
+  }
+};
